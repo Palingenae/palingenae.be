ae2fda503aace35941d879f74a391736e282109b
Olivia D. S <6728594+Palingenae@users.noreply.github.com>
2023-08-13 18:38:26 +0200

    [v0.1] First elements for a MVP (#1)
    * UPDATE : Hero on Index
    
    * UPDATE : Black and White
    
    * FIX: Removing that (lol)
    
    * FIX: Removing that too (lol)
    
    * UPDATE: Hardcoded content, light and black mode tweaks, responsive
    
    * UPDATE: Removing Strapi
    
    * UPDATE: More styling and responsive tweaks
    
    * UPDATE: More styling, responsive tweaks and content
    
    * UPDATE: More styling, responsive tweaks and 404 page
    
    * FIX: consistency
    
    * FIX: consistency
    
    * Update: hero and text tweaks
    
    * Update: Stylelint

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import prefetch from "@astrojs/prefetch";
+
+import purgecss from "astro-purgecss";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [prefetch(), purgecss()]
+});
