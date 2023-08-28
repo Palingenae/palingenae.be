136f3969d33714d844e382d543f3c9a5377c27d6
Palingenae <hello@palingenae.be>
2023-08-28 22:29:29 +0200

    UPDATE : CMS Content + Project Collection

---
+++ backend/src/collections/Projects.ts
@@ -0,0 +1,179 @@
+import { CollectionConfig } from "payload/types";
+
+const Projects: CollectionConfig = {
+    slug: "projects",
+    admin: {
+        useAsTitle: "title",
+        preview: (doc, { locale }) => {
+            if (doc?.slug) {
+                return `http://localhost:3333/preview/projects/${doc.slug}?locale=${locale}`;
+            }
+
+            return null;
+        },
+    },
+    access: {
+        read: () => true
+    },
+    fields: [
+        {
+            label: "Nom du projet",
+            name: "title",
+            type: "text",
+            required: true
+        },
+        {
+            label: "Slug",
+            name: "slug",
+            type: "text",
+            required: true,
+            unique: true
+        },
+        {
+            label: "Année",
+            name: "year",
+            type: "text",
+            required: true
+        },
+        {
+            label: "Type de projet",
+            name: "type",
+            type: "select",
+            options: [
+                {
+                    label: "Site web",
+                    value: "Site web"
+                },
+                {
+                    label: "API",
+                    value: "API"
+                }
+            ],
+            hasMany: false,
+            required: true
+        },
+        {
+            label: "Statut",
+            name: "status",
+            type: "select",
+            options: [
+                {
+                    label: "Actif",
+                    value: "Actif"
+                },
+                {
+                    label: "Terminé",
+                    value: "Terminé"
+                },
+            ],
+            hasMany: false,
+            required: true
+        },
+        {
+            label: "Périmètre",
+            name: "scope",
+            type: "select",
+            options: [
+                {
+                    label: "Design",
+                    value: "Design"
+                },
+                {
+                    label: "Design System",
+                    value: "Design System"
+                },
+                {
+                    label: "Intégration",
+                    value: "Intégration"
+                },
+                {
+                    label: "Front-End",
+                    value: "Front-End"
+                },
+                {
+                    label: "Back-End",
+                    value: "Back-End"
+                },
+                {
+                    label: "API",
+                    value: "API"
+                },
+                {
+                    label: "Hébergement",
+                    value: "Hébergement"
+                },
+            ],
+            hasMany: true,
+            required: true
+        },
+        {
+            label: "Outils",
+            name: "stack",
+            type: "select",
+            hasMany: true,
+            required: true,
+            // custom: true,
+            // admin: {
+            //     isClearable: true,
+            //     isSortable: true
+            // },
+            options: [
+                {
+                    label: "Figma",
+                    value: "Figma"
+                },
+                {
+                    label: "Astro",
+                    value: "Astro"
+                },
+                {
+                    label: "PayloadCMS",
+                    value: "PayloadCMS"
+                },
+                {
+                    label: "Sass",
+                    value: "Sass"
+                },
+                {
+                    label: "JavaScript",
+                    value: "JavaScript"
+                },
+                {
+                    label: "TypeScript",
+                    value: "TypeScript"
+                },
+                {
+                    label: "React",
+                    value: "React"
+                },
+                {
+                    label: "PHP",
+                    value: "PHP"
+                },
+                {
+                    label: "Symfony",
+                    value: "Symfony"
+                },
+                {
+                    label: "MySQL",
+                    value: "MySQL"
+                },
+                {
+                    label: "MongoDB",
+                    value: "MongoDB"
+                },
+                {
+                    label: "Docker",
+                    value: "Docker"
+                },
+            ],
+        },
+        {
+            label: "Contenu",
+            name: "article",
+            type: "richText",
+        }
+    ]
+}
+
+export default Projects;
