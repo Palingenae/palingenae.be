807eb631c285e081a09d136fa3dde862c41fff69
Olivia D. S <6728594+Palingenae@users.noreply.github.com>
2023-11-05 23:49:42 +0100

    Adding Blog Content-Type (#6)
    * UPDATE: Adding the blog
    
    * UPDATE: Adding and refactoring to a more generic terminology
    
    * UPDATE: spacing and sizing tweaks
    
    * UPDATE: adding react
    
    * UPDATE: Code Quality for JavaScript/TypeScript/React
    
    * UPDATE: Types for Front-End
    
    * UPDATE: Spacing and Sizing tweaks
    
    * UPDATE: Plugging into the CMS
    
    * UPDATE: Site version
    
    * UPDATE: Curriculum Tweaks
    
    * UPDATE: Code quality, readability, etc
    
    * UPDATE: Spacing and sizing tweaks
    
    * UPDATE: More code quality
    
    * UPDATE: removing useless content
    
    * UPDATE: error management if Payload fails?
    
    * UPDATE: new header component
    
    * UPDATE: version
    
    * FIX: building
    
    * FIX: Message classes
    
    * UPDATE: version
    
    * UPDATE: Adding Prettier
    
    * UPDATE: error mgmt, path and image in richtext
    
    * FIX: links and message

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import prefetch from "@astrojs/prefetch";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [prefetch()],
+  integrations: [prefetch(), react()]
 });
---
+++ types/Media.d.ts
@@ -0,0 +1,12 @@
+export interface Media {
+  id: string;
+  alt: string;
+  updatedAt: string;
+  createdAt: string;
+  url?: string;
+  filename?: string;
+  mimeType?: string;
+  filesize?: number;
+  width?: number;
+  height?: number;
+}
