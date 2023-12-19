59d6521156d56cf4fdc1f47044a84ea3baebca56
Olivia D. S <hello@palingenae.be>
2023-12-19 13:21:22 +0100

    UPDATE: Adding SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import prefetch from "@astrojs/prefetch";
-
 import react from "@astrojs/react";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [prefetch(), react()]
+  integrations: [prefetch(), react()],
+  output: "server",
+  adapter: netlify()
 });
