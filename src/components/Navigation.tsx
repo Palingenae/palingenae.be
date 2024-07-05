4a5ca7bb08279a6b74f8fbe237fc7552cb48ebd6
Olivia D. S <hello@palingenae.be>
2024-07-05 12:45:30 +0200

    WIP: cleaning the new navbar for new features

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -14,23 +14,7 @@ export default function Navigation() {
                     <button type="button" data-theme-toggle aria-label="Changer le thème du site"><Icon iconName="half-moon" /></button>
                     <span><Icon iconName="translate" /> FR</span>
                 </div> */}
-                {/* <div className="navigation__vertical__toggler">
-                    <button type="button" className="navigation__vertical__toggler__label" aria-label="Accéder au menu vertical">Menu</button>
-                </div> */}
             </div>
-            {/* <div className="navigation__vertical">
-                <div className="navigation__vertical__menu">
-                    <p className="navigation__vertical__menu__title">Navigation</p>
-                    {
-                        // navLinks !== undefined ?
-                        // navLinks.Routes.map((link: any) => (
-                        //     <a className={currentPath === link.URL ? "link -navigation -vertical -active" : "link -navigation -vertical"} href={ link.URL }>{ link.Name }</a>
-                        // ))
-                        // :
-                        // <a className={currentPath === '/about' ? "link -navigation -vertical -active" : "link -navigation -vertical"} href="/about">A propos</a>
-                    }
-                </div>
-            </div> */}
         </nav>
     )
 }
