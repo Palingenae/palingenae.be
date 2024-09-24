d590abf97ea501ee310c8d12754e4462ec9d83ad
Olivia De Scheerder <hey@palingenae.be>
2024-09-24 21:12:48 +0200

    UPDATE: aborted extensive redesign for now

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -7,13 +7,13 @@ export default function Navigation() {
                         <span className="siteName">Retourner à l'accueil</span>
                     </a>
                 </div>
-                { 
                 <div className="navigation__horizontal">
+                    <a className="link -ui" href="/about">À propos</a>
                         <button type="button" data-theme-toggle className="themeToggler"><i data-theme-icon className="icon iconoir-half-moon" aria-hidden> </i></button>
 
                     {/* Seeing this here? Yeah, it will be available later :)
                     <span><Icon iconName="translate" /> FR</span> */}
-                </div> }
+                </div>
             </div>
         </nav>
     )
